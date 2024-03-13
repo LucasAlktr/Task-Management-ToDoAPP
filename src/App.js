11b20951977df4f162d782e2bf659893c94178cb
LucasAlktr <lucasbenington@hotmail.com>
2024-03-13 15:03:35 -0400

    help and index addition

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
 import Header from "./components/Header/Header";
 import Tasks from "./components/Tasks/Tasks";
 import Form from "./components/Form/Form";
+import IndexHelp from "./components/Help/IndexHelp";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
 import { useState } from "react";
 import uuid from "react-uuid";
+import { Routes, Route } from "react-router-dom";
+
 
 function App() {
-
   const [tasks, setTasks] = useState([
     {
       id: uuid(),
-      description: 'Walk the Dog',
-      status: false
+      description: "Walk the Dog",
+      status: false,
     },
     {
       id: uuid(),
-      description: 'Wash the car',
-      status: false
+      description: "Wash the car",
+      status: false,
     },
     {
       id: uuid(),
-      description: 'Finish the lab',
-      status: false
-    }
+      description: "Finish the lab",
+      status: false,
+    },
   ]);
 
   const handleClearTasks = () => {
     setTasks([]);
-  }
+  };
 
   const handleStatusChange = (id) => {
     const updatedTasks = [...tasks];
@@ -36,34 +39,43 @@ function App() {
       }
     });
     setTasks(updatedTasks);
-  }
+  };
 
   const handleTaskRemove = (id) => {
-    const filteredTasks = tasks.filter(
-      (task) => task.id !== id
-    );
+    const filteredTasks = tasks.filter((task) => task.id !== id);
     setTasks(filteredTasks);
-  }
+  };
 
   const handleAddTask = (description, status) => {
-    setTasks([...tasks, {
-      id: uuid(), description: description, done: status === 'completed'
-    }]);
-
-  }
+    setTasks([
+      ...tasks,
+      {
+        id: uuid(),
+        description: description,
+        done: status === "completed",
+      },
+    ]);
+  };
 
   return (
     <>
       <Header />
-      <Tasks
-        tasks={tasks}
-        onStatusChange={handleStatusChange}
-        onTaskRemove={handleTaskRemove}
-        onClearTasks={handleClearTasks}
-      />
-      <Form 
-        onAddTask = {handleAddTask}
-      />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Tasks
+              tasks={tasks}
+              onStatusChange={handleStatusChange}
+              onTaskRemove={handleTaskRemove}
+              onClearTasks={handleClearTasks}
+            />
+          }
+        />
+        <Route path="/add" element={<Form onAddTask={handleAddTask} />} />
+        <Route path="/help" element={<IndexHelp />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
     </>
   );
 }
