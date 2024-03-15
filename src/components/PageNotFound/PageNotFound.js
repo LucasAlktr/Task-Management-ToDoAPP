a3fd16deb7b4dc4dc1b15c99501c848c552d5cc3
LucasAlktr <lucasbenington@hotmail.com>
2024-03-14 22:29:11 -0400

    pagenotfound page added

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,10 +1,12 @@
 import React from "react";
+import './style.scss'
 
 export default function PageNotFound() {
     return(
     <>
-        <div>
-            <h1>Page Not Found</h1>
+        <div className="container-error-page">
+            <h1 className="title-error-page">Page Not Found</h1>
+            <p className="text-error-page"><b>Oh no!</b> It seems that this page does not exist. Please verify the address.</p>
         </div>
     </>
     );
