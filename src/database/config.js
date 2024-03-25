b7c5b4520842be53e2277f493cf87b6cec1d4605
LucasAlktr <lucasbenington@hotmail.com>
2024-03-25 14:02:03 -0400

    adding firebase config

---
+++ src/database/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBS3idqHjcRzPx2SEjuBiL3L7KvB-wXTls",
+  authDomain: "taskmanagement-b2881.firebaseapp.com",
+  projectId: "taskmanagement-b2881",
+  storageBucket: "taskmanagement-b2881.appspot.com",
+  messagingSenderId: "625859037064",
+  appId: "1:625859037064:web:438b4e6fef44cd3d46dc3d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
