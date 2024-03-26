549d2aa2de2bb7326dd4f88eb948580657c1e983
LucasAlktr <lucasbenington@hotmail.com>
2024-03-26 01:02:03 -0400

    starting to implement update

--- src/database/add.js
+++ src/database/add.js
@@ -2,7 +2,6 @@ import { collection, addDoc } from "firebase/firestore";
 import { db } from "./config";
 
 export async function save (data){
-    console.log('File Added');
     
     try {
       const docRef = await addDoc(collection(db, "tasks"), data);
--- src/database/update.js
+++ src/database/update.js
@@ -1,3 +1,10 @@
-export function updateTask (){
-    console.log('File Updated')
+import { doc, updateDoc } from "firebase/firestore";
+
+export async function updateTask() {
+  console.log("File Updated");
+
+  const washingtonRef = doc(db, "cities", "DC");
+  await updateDoc(washingtonRef, {
+    capital: true,
+  });
 }
