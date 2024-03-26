7d9689136757019051fb7d04d695bd6831b4a0ad
LucasAlktr <lucasbenington@hotmail.com>
2024-03-26 01:05:14 -0400

    update database

--- src/database/update.js
+++ src/database/update.js
@@ -1,11 +1,9 @@
 import { doc, updateDoc } from "firebase/firestore";
 import { db } from "./config";
 
-export async function updateTask() {
+export async function updateTask(id, data) {
   console.log("File Updated");
 
-  const washingtonRef = doc(db, "cities", "DC");
-  await updateDoc(washingtonRef, {
-    capital: true,
-  });
+  const docRef = doc(db, "tasks", id);
+  await updateDoc(docRef, data);
 }
