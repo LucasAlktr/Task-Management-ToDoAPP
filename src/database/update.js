24fe314474f255703d2934c922aa114f06a4605d
LucasAlktr <lucasbenington@hotmail.com>
2024-03-26 01:06:58 -0400

    updating message

--- src/database/update.js
+++ src/database/update.js
@@ -2,7 +2,7 @@ import { doc, updateDoc } from "firebase/firestore";
 import { db } from "./config";
 
 export async function updateTask(id, data) {
-  console.log("File Updated");
+  console.log("Updating...");
 
   const docRef = doc(db, "tasks", id);
   await updateDoc(docRef, data);
